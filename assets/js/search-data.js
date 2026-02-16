63acbb8877ecbe4859838655da17ee1b367e5ba2
zhongyuan0 <sunzhongyuan2025@gmail.com>
2026-02-16 03:12:22 +0000

    Deploying to gh-pages from @ zhongyuan0/zhongyuan0.github.io@75ed7681cc742921a77fae036ce9ac7fc72a797a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%32%35%33%31%33%35%72@%72.%68%69%74-%75.%61%63.%6A%70", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/false", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=P6aIVc0AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
